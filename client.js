3b0a5fc441c5b2c6a437167865b0c9d8494e31cd
Noelani West <noelani.rose14@gmail.com>
2022-09-20 13:40:54 -0500

    Done

--- client.js
+++ client.js
@@ -52,7 +52,7 @@ for(let em of employees){
 //
 function calculateIndividualEmployeeBonus( employee ) {  
   // your logic here
-  let employeeLength = employee.employeeNumber.length;
+  // let employeeLength = employee.employeeNumber.length;
   let employeeBonus = {
     name: employee.name,
     // bonusPercentage: 0, 
@@ -62,61 +62,63 @@ function calculateIndividualEmployeeBonus( employee ) {
     // totalBonus: 0,
     // // Math.ceil(totalCompensation - employee.annualSalary)
   };
+  
   // Start of bonusPercentage if
-  // if (employees.employeeNumber<10000){
-    if (employee.reviewRating <= 2){
-      employeeBonus.bonusPercentage = 1;
-    }
-    else if (employee.reviewRating === 3){
-      employeeBonus.bonusPercentage = 1.04;
-    }
-    else if(employee.reviewRating===4){
-      employeeBonus.bonusPercentage=1.06;
-    }
-    else{
-      employeeBonus.bonusPercentage=1.10;
-    };
-      if (employeeLength < 5 ){
-        employeeBonus.bonusPercentage += .05;
-      }
-  // else{
-  //   if (employee.reviewRating <= 2){
-  //     employeeBonus.bonusPercentage = 1;
-  //   }
-  //   else if (employee.reviewRating === 3){
-  //     employeeBonus.bonusPercentage = 1.04;
-  //   }
-  //   else if(employee.reviewRating===4){
-  //     employeeBonus.bonusPercentage=1.06;
-  //   }
-  //   else{
-  //     employeeBonus.bonusPercentage=1.10;
-  //   };
-    
-  //   if (employees.employeeNumber<10000){
-  //     employeeBonus.bonusPercentage +=.05
-  //   };
-  // };
+  if (employee.reviewRating <= 2){
+    employeeBonus.bonusPercentage = 1;
+  }
+  else if (employee.reviewRating === 3){
+    employeeBonus.bonusPercentage = 1.04;
+  }
+  else if(employee.reviewRating===4){
+    employeeBonus.bonusPercentage=1.06;
+  }
+  else{
+    employeeBonus.bonusPercentage=1.1;
+  };
+  
+  if (employee.employeeNumber<10000 ){
+    employeeBonus.bonusPercentage += .05;
+  };
+  
+  if(employee.annualSalary>65000 && employeeBonus.bonusPercentage>1){
+    employeeBonus.bonusPercentage=employeeBonus.bonusPercentage-=.01
+  };
+  
+  if(employeeBonus.bonusPercentage>1.13){
+    employeeBonus.bonusPercentage=1.13
+  }
+  
   //end of the bonusPercentage if
   
   //create total compensation attribute
   
-  employeeBonus.totalCompesation=employee.annualSalary * employeeBonus.bonusPercentage
+  employeeBonus.totalCompesation=employee.annualSalary * employeeBonus.bonusPercentage;
+  
   
   // total compensation attribute created
   //create total bonus
+  
   employeeBonus.totalBonus=Math.ceil((employee.annualSalary * employeeBonus.bonusPercentage) - employee.annualSalary);
+  
   // return new object with bonus results
-  console.log(employeeLength);
-
+  // console.log(employeeLength);
+  // employeeBonus.bonusPercentage=employeeBonus.bonusPercentage-1;
+  
+  employeeBonus.bonusPercentage=String(employeeBonus.bonusPercentage).slice(-1);
+  employeeBonus.bonusPercentage=employeeBonus.bonusPercentage+'%';
   return employeeBonus;
 }
-console.log(calculateIndividualEmployeeBonus(employees[0]));
-console.log(calculateIndividualEmployeeBonus(employees[1]));
-console.log(calculateIndividualEmployeeBonus(employees[2]));
-console.log(calculateIndividualEmployeeBonus(employees[3]));
-console.log(calculateIndividualEmployeeBonus(employees[4]));
 
 
+// console.log(calculateIndividualEmployeeBonus(employees));
+// console.log(calculateIndividualEmployeeBonus(employees[1]));
+// console.log(calculateIndividualEmployeeBonus(employees[2]));
+// console.log(calculateIndividualEmployeeBonus(employees[3]));
+// console.log(calculateIndividualEmployeeBonus(employees[4]));
 
+
+for(let people of employees ){
+  console.log(calculateIndividualEmployeeBonus(people));
+};
 
