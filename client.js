89ce01cd7f00d5f4857f69f1bd77dd224ad3066b
Noelani West <noelani.rose14@gmail.com>
2022-09-20 11:17:49 -0500

    working on adding to employeeBonus

--- client.js
+++ client.js
@@ -43,15 +43,78 @@ console.log('array of employee data: ',  employees );
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-
+for(let em of employees){
+  console.log(em);
+}
 
 
 // This function will calculate 1 employee's bonus!
 //
 function calculateIndividualEmployeeBonus( employee ) {  
   // your logic here
+  let percentages = 0;
+  let employeeBonus = {
+    name: employee.name,
+    // bonusPercentage: 0, 
+    // // percentages,
+    // totalCompensation: 0,
+    // // employee.annualSalary * employee.bonusPercentage,
+    // totalBonus: 0,
+    // // Math.ceil(totalCompensation - employee.annualSalary)
+  };
+  // Start of bonusPercentage if
+  // if (employees.employeeNumber<10000){
+    if (employee.reviewRating <= 2){
+      employeeBonus.bonusPercentage = 1;
+    }
+    else if (employee.reviewRating === 3){
+      employeeBonus.bonusPercentage = 1.04;
+    }
+    else if(employee.reviewRating===4){
+      employeeBonus.bonusPercentage=1.06;
+    }
+    else{
+      employeeBonus.bonusPercentage=1.10;
+    };
+      if (employee.employeeNumber < 10000 ){
+        employee.employeeBonus = employee.employeeBonus += .05;
+      }
+  // else{
+  //   if (employee.reviewRating <= 2){
+  //     employeeBonus.bonusPercentage = 1;
+  //   }
+  //   else if (employee.reviewRating === 3){
+  //     employeeBonus.bonusPercentage = 1.04;
+  //   }
+  //   else if(employee.reviewRating===4){
+  //     employeeBonus.bonusPercentage=1.06;
+  //   }
+  //   else{
+  //     employeeBonus.bonusPercentage=1.10;
+  //   };
+    
+  //   if (employees.employeeNumber<10000){
+  //     employeeBonus.bonusPercentage +=.05
+  //   };
+  // };
+  //end of the bonusPercentage if
   
+  //create total compensation attribute
   
+  employeeBonus.totalCompesation=employee.annualSalary * employeeBonus.bonusPercentage
+  
+  // total compensation attribute created
+  //create total bonus
+  employeeBonus.totalBonus=Math.ceil((employee.annualSalary * employeeBonus.bonusPercentage) - employee.annualSalary);
   // return new object with bonus results
-
+  
+  return employeeBonus;
 }
+console.log(calculateIndividualEmployeeBonus(employees[0]));
+console.log(calculateIndividualEmployeeBonus(employees[1]));
+console.log(calculateIndividualEmployeeBonus(employees[2]));
+console.log(calculateIndividualEmployeeBonus(employees[3]));
+console.log(calculateIndividualEmployeeBonus(employees[4]));
+
+
+
